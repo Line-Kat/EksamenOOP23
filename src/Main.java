812b32ae8bfc213ff0be639cd5ecf8ce35c7318e
L_Kat <l_katralen@hotmail.com>
2023-06-14 12:11:42 +0200

    added scanner and class EventManagement

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
 
     //TODO remember the README file
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
     }
 }
